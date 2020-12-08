b1a707a13dd1755c5bc9866bdc38f43562d3c2e2
BryanNearing <bryannearing@gmail.com>
2020-12-08 15:36:35 -0500

    accidentally deleted a couple of things

--- src/longIncSubPack/LongestIncreasingSubsequence.java
+++ src/longIncSubPack/LongestIncreasingSubsequence.java
@@ -26,12 +26,17 @@ public class LongestIncreasingSubsequence {
 			lis[i] = 1;
 		}
 		
+		for(int i = 0; i < n - 1; i++) {
+			System.out.print(arr[i] + ",");
+		}
+		System.out.println(arr[n - 1]);
+		
 		for(int i = 1; i < arr.length; i++) {
 			for(int j = 0; j < i; j++) {
 				if(arr[j] < arr[i] && lis[i] < lis[j] + 1) {
+					lis[i] = lis[j] + 1;
 				}
 			}
-			System.out.println();
 		}
 		
 		int seq = 0;
